0bf2bdd2e65d2b596a17c7d24518db6fe256c21f
Robson <robsonad07@gmail.com>
2024-05-18 19:11:23 -0300

    Corrindo alguns erro de design

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Admin/footer/Footer";
-import Header from "./components/Admin/header/Header";
 import Home from "./pages/admin/home/Home";
-import Sobre from "./pages/admin/sobre/Sobre";
+import Sobre from "./pages/user/sobre/Sobre";
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Header />
-
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/admin/home" element={<Home />} />
           <Route path="/sobre" element={<Sobre />} />
         </Routes>
-
-        <Footer />
       </BrowserRouter>
     </>
   );
