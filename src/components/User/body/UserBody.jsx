c9e38fffe93d4e4ab504fad2eb5a8d62c8cd4f44
Dhymas Gabriel <dhymas.g@gmail.com>
2024-05-23 16:02:17 -0300

    Ajuste de Responsividade

--- src/components/User/body/UserBody.jsx
+++ src/components/User/body/UserBody.jsx
@@ -7,18 +7,16 @@ export default function UserBody() {
       image: 'https://images.unsplash.com/photo-1591492654773-6756035bef6b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       title: 'Eventos de Literatura',
       description: 'Os eventos culturais de literatura são dedicados aos amantes da escrita e leitura, proporcionando uma imersão no mundo das palavras.',
-    
     },
     {
       image: 'https://images.unsplash.com/photo-1415886541506-6efc5e4b1786?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       title: 'Eventos Urbanos',
       description: 'Prepare-se para mergulhar na vibrante atmosfera das culturas urbanas com os eventos dirigidos pela equipe do renomado DJ Jheyeldo.',
-      
     }
   ];
 
   return (
-    <div className="max-w-7xl mx-auto py-12 px-4">
+    <div className="max-w-7xl mx-auto py-12 px-4 overflow-x-hidden">
       <h2 className="text-3xl font-bold mb-8 text-center">Eventos Culturais</h2>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
         {events.map((event, index) => (
@@ -27,11 +25,9 @@ export default function UserBody() {
             image={event.image}
             title={event.title}
             description={event.description}
-            link={event.link}
           />
         ))}
       </div>
     </div>
   );
 }
-
--- src/pages/user/sobre/Sobre.jsx
+++ src/pages/user/sobre/Sobre.jsx
@@ -1,51 +1,46 @@
 import UserFooter from "../../../components/User/Footer/UserFooter";
 import UserHeader from "../../../components/User/header/UserHeader";
 
-
 export default function Sobre() {
 	return (
-		<div className="flex flex-col min-h-screen bg-white">
+		<div className="flex flex-col min-h-screen bg-white overflow-x-hidden">
 			<UserHeader />
-			<div className="flex-grow p-6">
+			<div className="flex-grow p-6 max-w-full">
 				<div className="w-full bg-neutral-50 rounded-lg shadow-lg p-6">
 					<h1 className="sm:text-6xl text-4xl text-center font-black mb-16">
 						<span className="text-orange-300">Event</span>Hub
 					</h1>
-					<h2 className="text-2xl font-black mb-8">Olá <span className="text-orange-300">Visitante</span>, Bem-vindo! </h2>
+					<h2 className="text-2xl font-bold mb-8">Olá <span className="text-orange-300">Visitante</span>, Bem-vindo! </h2>
 					<p className="text-xl font-semibold">
 						Cansado de perder os melhores eventos da sua região?
 
 						O EventHub é a sua solução completa para encontrar e gerenciar eventos culturais imperdíveis!
 					</p>
 					<br />
-					<h3 className="text-2xl font-black">Com o <span className="text-orange-300">Event</span>Hub, você pode:</h3>
-					<p>
-						<ul className="list-inside list-none md:list-disc text-xl font-semibold">
-							<li>
-								Descobrir eventos em sua área de interesse, como shows, peças de teatro, exposições de arte e muito mais.
-							</li>
-							<li>
-								Filtrar eventos por data, local e categoria para encontrar exatamente o que você procura.
-							</li>
-						</ul>
-					</p>
+					<h3 className="text-2xl font-bold">Com o <span className="text-orange-300">Event</span>Hub, você pode:</h3>
+					<ul className="list-inside list-none md:list-disc text-xl font-semibold pl-4 md:pl-0">
+						<li>
+							Descobrir eventos em sua área de interesse, como shows, peças de teatro, exposições de arte e muito mais.
+						</li>
+						<li>
+							Filtrar eventos por data, local e categoria para encontrar exatamente o que você procura.
+						</li>
+					</ul>
 					<br />
-					<h3 className="text-2xl font-black">
+					<h3 className="text-2xl font-bold">
 						O <span className="text-orange-300">Event</span>Hub é a ferramenta perfeita para quem:
 					</h3>
-					<p>
-						<ul className="list-inside list-none md:list-disc text-xl font-semibold">
-							<li>
-								Ama eventos culturais e quer se manter atualizado sobre as últimas novidades.
-							</li>
-							<li>
-								Está sempre em busca de novas experiências e quer descobrir eventos diferentes.
-							</li>
-							<li>
-								Quer economizar tempo na busca por novos eventos.
-							</li>
-						</ul>
-					</p>
+					<ul className="list-inside list-none md:list-disc text-xl font-semibold pl-4 md:pl-0">
+						<li>
+							Ama eventos culturais e quer se manter atualizado sobre as últimas novidades.
+						</li>
+						<li>
+							Está sempre em busca de novas experiências e quer descobrir eventos diferentes.
+						</li>
+						<li>
+							Quer economizar tempo na busca por novos eventos.
+						</li>
+					</ul>
 					<br />
 					<p className="text-xl font-black">
 						Este é um projeto desenvolvido pela equipe HTTP Heroes durante o bootcamp de Desenvolvimento FullStack da Atlântico.
